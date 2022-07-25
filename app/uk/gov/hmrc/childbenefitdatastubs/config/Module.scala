33ff9f3a0ad535412e4da509c10495ae6fc53b6f
Prasad Sriramula <psriramula@icloud.com>
2022-07-25 13:33:49 +0100

    SB-729- adding data for payment

--- app/uk/gov/hmrc/childbenefitdatastubs/config/Module.scala
+++ app/uk/gov/hmrc/childbenefitdatastubs/config/Module.scala
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ */
+
 package uk.gov.hmrc.childbenefitdatastubs.config
 
 import com.google.inject.AbstractModule
--- app/uk/gov/hmrc/childbenefitdatastubs/controllers/ChildBenefitFinancialDetailsController.scala
+++ app/uk/gov/hmrc/childbenefitdatastubs/controllers/ChildBenefitFinancialDetailsController.scala
@@ -11,10 +11,20 @@ import javax.inject.{Inject, Singleton}
 import scala.concurrent.Future
 
 @Singleton()
-class MicroserviceHelloWorldController @Inject()(cc: ControllerComponents)
+class ChildBenefitFinancialDetailsController @Inject()(cc: ControllerComponents)
     extends BackendController(cc) {
 
   def hello(): Action[AnyContent] = Action.async { implicit request =>
     Future.successful(Ok("Hello world"))
   }
+
+  /*
+    Example URIs
+    /child-benefit/financial-details/AB654321
+    /child-benefit/financial-details/YY123499D
+   */
+  def financialDetails(correlationId:String ): Action[AnyContent] = Action.async { implicit r =>
+    Future.successful(Ok("Hello world"))
+  }
+
 }
