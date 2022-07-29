94cf2e6d52456d41fee28dd923a87e4a334b8bb8
Prasad Sriramula <psriramula@icloud.com>
2022-07-29 11:34:57 +0100

    SB-729- adding data for payment-resolved compilation and run issues

---
+++ app/childbenefitstubs/DESDataStubController.scala
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ */
+
+package childbenefitstubs
+
+import ch.qos.logback.core.util.FileUtil
+
+import java.time.LocalDate.now
+import java.time.format.DateTimeFormatter.ISO_DATE
+import scala.collection.mutable
+import play.api.mvc._
+import play.api.Logger
+import play.api.libs.json.JsValue
+import play.api.libs.json.Json.toJson
+
+import javax.inject.{Inject, Singleton}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendController
+import util.FileUtils
+
+@Singleton()
+class DESDataStubController @Inject() (mcc: MessagesControllerComponents) extends FrontendController(mcc) {
+
+  def paymentDetails(identifier: String): Action[AnyContent] = Action { implicit request =>
+
+    val content = FileUtils.readContent("des/api/child-benefit-financial-details", identifier)
+
+    Ok(content).withHeaders("CorrelationId" -> "a1e8057e-fbbc-47a8-a8b4-78d9f015c253", "Content-Type" -> "application/json")
+
+  }
+
+  def relationship(idNumber: String): Action[AnyContent] = Action { implicit request =>
+
+    val content = FileUtils.readContent("des/api/individuals-relationship-details", idNumber)
+
+    Ok(content).withHeaders("CorrelationId" -> "a1e8057e-fbbc-47a8-a8b4-78d9f015c253", "Content-Type" -> "application/json")
+
+  }
+
+  def individualsDetails(idNumber: String): Action[AnyContent] = Action { implicit request =>
+
+    val content = FileUtils.readContent("des/api/individuals-details", idNumber)
+
+    Ok(content).withHeaders("CorrelationId" -> "a1e8057e-fbbc-47a8-a8b4-78d9f015c253", "Content-Type" -> "application/json")
+
+  }
+
+}
