6e81ce3d41f7c9a0c7c337caef87d2157f753cd4
Divilioglu <mo.divilioglu@gmail.com>
2022-09-15 15:39:37 +0100

    SB-777 - Rebased and fixed test

--- test/NoAccountFoundControllerSpec.scala
+++ test/NoAccountFoundControllerSpec.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import controllers.routes
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
@@ -16,7 +32,7 @@ class NoAccountFoundControllerSpec extends BaseISpec {
 
       running(application) {
         val request = FakeRequest(GET, routes.NoAccountFoundController.onPageLoad.url)
-
+          .withSession(("authToken", "Bearer 123"))
         val result = route(application, request).value
 
         val view = application.injector.instanceOf[NoAccountFoundView]
@@ -28,19 +44,20 @@ class NoAccountFoundControllerSpec extends BaseISpec {
     }
 
     "must return UNAUTHORISED and the correct view for a GET" in {
-      afterReset {
-        val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
 
-        running(application) {
-          val request = FakeRequest(GET, routes.NoAccountFoundController.onPageLoad.url)
+      userLoggedInChildBenefitUser(NinoUser)
+      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
 
-          val result = route(application, request).value
+      running(application) {
+        val request = FakeRequest(GET, routes.NoAccountFoundController.onPageLoad.url)
 
-          status(result) mustEqual SEE_OTHER
-          redirectLocation(result).value must startWith(application.configuration.get[String]("urls.login"))
+        val result = route(application, request).value
 
-        }
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value must startWith(application.configuration.get[String]("urls.login"))
+
       }
+
     }
   }
 }
