3a98bbd453e0ad24c119442d67e2a04907029476
user <mo.divilioglu@gmail.com>
2022-10-20 13:25:03 +0100

    SB-959 - Create Page (English) - Page not found (404 error)

--- test/controllers/NoAccountFoundControllerSpec.scala
+++ test/controllers/NoAccountFoundControllerSpec.scala
@@ -14,12 +14,29 @@
  * limitations under the License.
  */
 
-import controllers.routes
+package controllers
+
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
-import utils.Stubs.userLoggedInChildBenefitUser
 import utils.BaseISpec
 import utils.NonceUtils.removeNonce
+import utils.Stubs.userLoggedInChildBenefitUser
 import utils.TestData.NinoUser
 import views.html.NoAccountFoundView
 
