a2af1c4e3b2d855dde2474a7077d45452b310f71
Daniel Lackenby <11579453+KiraNamida@users.noreply.github.com>
2022-12-05 08:59:14 +0000

    SB-1210: Format user name and address for the front end (#94)
    * utils to format ChildBenefitEntitlement for front end presentation
    
    * Leverage testcases as val to reduce number of methods in spec files
    
    * Reduce repeated code for Address Line test cases and move all test cases into companion object to break up the large test file
    
    * Forgot to run scala formatter

---
+++ app/utils/helpers/ChildBenefitEntitlementHelper.scala
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package utils.helpers
+
+import models.common.{AddressLine, AddressPostcode}
+import models.entitlement.{ChildBenefitEntitlement, FullName}
+import utils.helpers.StringHelper.toTitleCase
+
+object ChildBenefitEntitlementHelper {
+  val defaultDelimiters    = List(" ", "-", "'")
+  val defaultExceptedWords = List("and")
+
+  def formatChildBenefitEntitlement(entitlement: ChildBenefitEntitlement): ChildBenefitEntitlement =
+    entitlement.copy(
+      claimant = entitlement.claimant.copy(
+        name = FullName(toTitleCase(entitlement.claimant.name.value, defaultDelimiters, defaultExceptedWords)),
+        fullAddress = entitlement.claimant.fullAddress.copy(
+          addressLine1 = AddressLine(
+            toTitleCase(entitlement.claimant.fullAddress.addressLine1.value, defaultDelimiters, defaultExceptedWords)
+          ),
+          addressLine2 = AddressLine(
+            toTitleCase(entitlement.claimant.fullAddress.addressLine2.value, defaultDelimiters, defaultExceptedWords)
+          ),
+          addressLine3 = entitlement.claimant.fullAddress.addressLine3.fold[Option[AddressLine]](None)(al3 =>
+            Some(AddressLine(toTitleCase(al3.value, defaultDelimiters, defaultExceptedWords)))
+          ),
+          addressLine4 = entitlement.claimant.fullAddress.addressLine4.fold[Option[AddressLine]](None)(al4 =>
+            Some(AddressLine(toTitleCase(al4.value, defaultDelimiters, defaultExceptedWords)))
+          ),
+          addressLine5 = entitlement.claimant.fullAddress.addressLine5.fold[Option[AddressLine]](None)(al5 =>
+            Some(AddressLine(toTitleCase(al5.value, defaultDelimiters, defaultExceptedWords)))
+          ),
+          addressPostcode = AddressPostcode(entitlement.claimant.fullAddress.addressPostcode.value.toUpperCase)
+        )
+      )
+    )
+}
