b32e0759ef4865b11cd293df130ef50c4aec0e56
Daniel Lackenby <11579453+KiraNamida@users.noreply.github.com>
2023-01-19 11:34:44 +0000

    Hide the back link element when JS is disabled

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,7 +5,7 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc"            % "3.21.0-play-28",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc"            % "3.29.0-play-28",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping" % "1.11.0-play-28",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"    % "5.24.0",
     "uk.gov.hmrc"       %% "play-language"                 % "5.3.0-play-28",
