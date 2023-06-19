660a0ec26449a287e320226b76a365805afca71d
Radek Hubner <rhubner@users.noreply.github.com>
2023-06-19 14:06:07 +0100

    SB-1747 - Add outage banner.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,12 +3,12 @@ import sbt._
 object AppDependencies {
   import play.core.PlayVersion
 
-  val HMRCMongoPlayVersion = "1.1.0"
-  val HMRCBoostrapPlayVersion = "7.15.0"
+  val HMRCMongoPlayVersion = "1.3.0"
+  val HMRCBoostrapPlayVersion = "7.18.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc"            % "7.5.0-play-28",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc"            % "7.12.0-play-28",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping" % "1.13.0-play-28",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"    % HMRCBoostrapPlayVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"            % HMRCMongoPlayVersion,
