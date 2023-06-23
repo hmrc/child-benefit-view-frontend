be7cab5dde048b9dec6ac0f16c7e37a0a8d0de85
Kritika Rai <111003013+kritikarai6@users.noreply.github.com>
2023-06-23 10:28:56 +0100

    upgrading the version

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -13,7 +13,7 @@ object AppDependencies {
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"    % HMRCBoostrapPlayVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"            % HMRCMongoPlayVersion,
     "org.typelevel"     %% "cats-core"                     % "2.9.0",
-    "uk.gov.hmrc"       %% "sca-wrapper"                   % "1.0.36"
+    "uk.gov.hmrc"       %% "sca-wrapper"                   % "1.0.37"
   )
 
   val test: Seq[ModuleID] = Seq(
