ccd1884ebcb366433bdb8cc8de2954566f0a1904
JoeSimmonds <884957+JoeSimmonds@users.noreply.github.com>
2024-02-02 13:42:08 +0000

    SO-276 |  Added test for CBDataRetrievalAction

---
+++ test/controllers/actions/CBDataRetrievalActionSpec.scala
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.actions
+
+import base.BaseAppSpec
+import models.UserAnswers
+import models.common.NationalInsuranceNumber
+import models.requests.{IdentifierRequest, OptionalDataRequest}
+import org.mockito.Mockito._
+import org.scalatestplus.mockito.MockitoSugar
+import play.api.test.FakeRequest
+import repositories.SessionRepository
+import stubs.AuthStubs._
+import utils.TestData.ninoUser
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+class CBDataRetrievalActionSpec extends BaseAppSpec with MockitoSugar {
+
+  class Harness(sessionRepository: SessionRepository) extends CBDataRetrievalActionImpl(sessionRepository) {
+    def callTransform[A](request: IdentifierRequest[A]): Future[OptionalDataRequest[A]] = transform(request)
+  }
+
+  "CB Data Retrieval Action" - {
+
+    "when there is no data in the cache" - {
+
+      "must set userAnswers to 'None' in the request" in {
+        userLoggedInIsChildBenefitUser(ninoUser)
+
+        val sessionRepository = mock[SessionRepository]
+        when(sessionRepository.get("id")) thenReturn Future(None)
+        val action = new Harness(sessionRepository)
+
+        val result = action
+          .callTransform(IdentifierRequest(FakeRequest(), NationalInsuranceNumber("QQ123456B"), true, "id"))
+          .futureValue
+
+        result.userAnswers must not be defined
+      }
+    }
+
+    "when there is data in the cache" - {
+
+      "must build a userAnswers object and add it to the request" in {
+        userLoggedInIsChildBenefitUser(ninoUser)
+
+        val sessionRepository = mock[SessionRepository]
+        when(sessionRepository.get("id")) thenReturn Future(Some(UserAnswers("id")))
+        val action = new Harness(sessionRepository)
+
+        val result = action
+          .callTransform(IdentifierRequest(FakeRequest(), NationalInsuranceNumber("QQ123456C"), true, "id"))
+          .futureValue
+
+        result.userAnswers.value.id mustBe "id"
+      }
+    }
+  }
+}
