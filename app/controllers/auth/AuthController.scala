cd9040df2066cb91a9e8e23d39ea435c8b6dd839
jmsatheesh <19515164+jmsatheesh@users.noreply.github.com>
2025-06-27 13:04:09 +0100

    reverted authcontroller changes

--- app/controllers/auth/AuthController.scala
+++ app/controllers/auth/AuthController.scala
@@ -31,17 +31,17 @@ import javax.inject.Inject
 import scala.concurrent.ExecutionContext
 
 class AuthController @Inject() (
-   sessionRepository: SessionRepository,
-   authConnector:     AuthConnector,
-   auth:              StandardAuthJourney
- )(implicit
-   config:            Configuration,
-   env:               Environment,
-   ec:                ExecutionContext,
-   cc:                MessagesControllerComponents,
-   frontendAppConfig: FrontendAppConfig
- ) extends ChildBenefitBaseController(authConnector)
-  with I18nSupport {
+    sessionRepository: SessionRepository,
+    authConnector:     AuthConnector,
+    auth:              StandardAuthJourney
+  )(implicit
+    config:            Configuration,
+    env:               Environment,
+    ec:                ExecutionContext,
+    cc:                MessagesControllerComponents,
+    frontendAppConfig: FrontendAppConfig
+  ) extends ChildBenefitBaseController(authConnector)
+   with I18nSupport {
 
   private val logger = new RequestLogger(this.getClass)
 
