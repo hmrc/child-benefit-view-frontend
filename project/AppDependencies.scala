177516065f8189ef14b08ddb14a32cdcc66183fe
DIGITAL317588 <17904833+smithrebecca@users.noreply.github.com>
2025-09-16 10:20:21 +0100

    DDCNL-10809 | upgrading bootstrap version

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,7 +2,7 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.13.0"
+  private val bootstrapVersion = "10.1.0"
   private val hmrcMongoVersion = "2.6.0"
   private val playVersion      = "play-30"
 
