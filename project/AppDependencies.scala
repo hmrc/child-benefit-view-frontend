2e195e61ad7801a3760b70369b74c7a41168d303
jesskay2 <114414761+jesskay2@users.noreply.github.com>
2025-10-22 20:13:29 +0100

    DDCNL-11122 | govUk rebrand and dependency update

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,15 +2,15 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.13.0"
-  private val hmrcMongoVersion = "2.6.0"
+  private val bootstrapVersion = "10.3.0"
+  private val hmrcMongoVersion = "2.10.0"
   private val playVersion      = "play-30"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
     play.sbt.PlayImport.caffeine,
     "org.typelevel"     %% "cats-core"                                  % "2.13.0",
-    "uk.gov.hmrc"       %% s"sca-wrapper-$playVersion"                  % "2.15.0",
+    "uk.gov.hmrc"       %% s"sca-wrapper-$playVersion"                  % "4.3.0",
     "uk.gov.hmrc"       %% s"mongo-feature-toggles-client-$playVersion" % "1.10.0"
   )
 
@@ -18,7 +18,7 @@ object AppDependencies {
     "uk.gov.hmrc"       %% s"bootstrap-test-$playVersion"  % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% s"hmrc-mongo-test-$playVersion" % hmrcMongoVersion,
     "org.scalatestplus" %% "scalacheck-1-18"               % "3.2.19.0",
-    "org.jsoup"          % "jsoup"                         % "1.20.1"
+    "org.jsoup"          % "jsoup"                         % "1.21.2"
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID] = compile ++ test
